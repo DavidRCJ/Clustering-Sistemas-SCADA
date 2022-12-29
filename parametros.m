a3844d983f757f68481eada2f11ee7dc424bfc1b
David <davidricardocruzjuarez@gmail.com>
2022-12-29 09:35:36 -0600

    Sistema de dos tanques interconectados e implementando interfaz SCADA con análisis de detección de eventos basado en modelado de datos clustering mediante algoritmos de Función discriminante y K-means

---
+++ parametros.m
@@ -0,0 +1,14 @@
+rng('default') % For reproducibility 
+%Definicion de constantes
+
+qi=3;
+klm=0.9580;
+kmr=1.3635;
+kr=1.8;
+al=95.52;
+am=64;
+ar=45;
+%Perturbaciones del sistema
+Tuberia_err=0;
+Nivel_err=0;
+Nivel2_err=0;
